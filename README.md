2ca040a88400ed0ab524139252a6d4add47504f4
Igor Kabacinski <igor.kabacinski@gmail.com>
2019-03-16 16:16:38 +0100

    Na poczatek

---
+++ README.md
@@ -0,0 +1,1 @@
+# Android Calculator in JAVA
---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.mycalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button simpleButton = (Button) findViewById(R.id.simpleButton);
+        simpleButton.setOnClickListener(this);
+
+        Button advancedButton = (Button) findViewById(R.id.advancedButton);
+        simpleButton.setOnClickListener(this);
+
+        Button aboutButton = (Button) findViewById(R.id.aboutButton);
+        simpleButton.setOnClickListener(this);
+
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        simpleButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.simpleButton: {
+
+                break;
+            }
+            case R.id.advancedButton: {
+
+                break;
+            }
+            case R.id.aboutButton: {
+
+                break;
+            }
+            case R.id.exitButton: {
+                finish();
+                System.exit(0);
+                break;
+            }
+        }
+    }
+}
